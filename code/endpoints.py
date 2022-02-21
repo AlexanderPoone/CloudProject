ab8f48240961e1c07df50485801d76006e64bb90
SoftFeta <mundanesurrealism@gmail.com>
2022-02-21 12:20:15 +0800

    .

--- code/endpoints.py
+++ code/endpoints.py
@@ -1,18 +1,52 @@
 '''
 Cloud Computing Project Client REST API
+海洋・湖沼・河川を航行する船舶
+ヴァン・ベートーヴェン
 '''
+import boto3
+import botocore
+import paramiko
 
 from flask import Flask, jsonify, request, send_from_directory, abort, send_file
 from flask_cors import CORS
 
+from hashlib import sha512
+from os import urandom
+from os.path import expanduser
+from time import sleep
+
 app = Flask(__name__)
 CORS(app)
 
+s = boto3.Session(region_name='us-east-1')
+client = s.resource('dynamodb')
+
+tableUsers = client.Table("users")
+print(tableUsers.table_status)
+tableDedupSequence = client.Table("dedupSequence")
+print(tableDedupSequence.table_status)
+
 @app.route('/login', methods = ['POST'])
 def login():
-	request.form['username']
-	request.form['hash']
-	return
+	record = tableUsers.get_item(Key={'userid': request.form['username']})
+	print(record)
+	if 'Item' in record:
+		print(record['Item'])
+
+		print(record['Item']['hash'] == request.form['hash'])
+		sessionid = 'blahblahblah'
+	else:
+		sessionid = False
+	return {'sessionid': sessionid}
+
+@app.route('/register', methods = ['POST'])
+def register():
+	
+	str(sha512(bytes(salt)).hexdigest())
+	sha512(bytes(salt)).hexdigest()
+	salt = urandom(16)
+	tableUsers.put_item(Item={'userid': request.form['username'], 'salt': salt, 'hash': request.form['hash']})
+	return True
 
 @app.route('/getFileList', methods = ['POST'])
 def getFileList():
@@ -20,16 +54,23 @@ def getFileList():
 	return
 
 @app.route('/getFile', methods = ['POST'])
-def getFileList():
+def getFile():
 	# Location
-
+	'segmentids'
 	# Assemble!
 	request.form['session']
 	request.form['fileId']
 	return
 
+@app.route('/upload', methods = ['POST'])
+def upload():
+	# Just split file into chunks
+	tableDedupSequence.put_item(Item={'userid': '35', 'sequence': 'microsoft'})
 
 @app.route('/logout', methods = ['POST'])
-def getFileList():
+def logout():
 	request.form['session']
 	return
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', ssl_context=('cert.pem', 'privkey.pem'), port=18888)
---
+++ code/flutterclient/lib/login_screen.dart
@@ -0,0 +1,260 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
+import 'package:flutter_login/flutter_login.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
+
+import 'constants.dart';
+import 'custom_route.dart';
+import 'dashboard_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const routeName = '/auth';
+
+  const LoginScreen({Key? key}) : super(key: key);
+  static var client = http.Client();
+
+  Duration get loginTime => Duration(milliseconds: timeDilation.ceil() * 2250);
+
+  Future<String?> _loginUser(LoginData data) {
+    return Future.delayed(loginTime).then((_) async {
+      var bytes = utf8.encode(data.password); // data being hashed
+      var digest = sha1.convert(bytes);
+
+      try {
+        var response = await client.post(
+            Uri.https('dord.mynetgear.com:18888', 'login'),
+            body: {'username': data.name, 'hash': digest.toString()});
+        var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+        print(decodedResponse);
+        // var uri = Uri.parse(decodedResponse['uri'] as String);
+        // print(await client.get(uri));
+        if (decodedResponse['sessionid'] == false) {
+          return 'User not exists or password does not match';
+        }
+      } finally {
+        client.close();
+      }
+      return null;
+    });
+  }
+
+  Future<String?> _signupUser(SignupData data) {
+    return Future.delayed(loginTime).then((_) {
+      var bytes = utf8.encode(data.hashCode); // data being hashed
+      var digest = sha1.convert(bytes);
+
+      try {
+        var response = await client.post(
+            Uri.https('dord.mynetgear.com:18888', 'register'),
+            body: {'username': data.name, 'hash': digest.toString()});
+        var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+        print(decodedResponse);
+      } finally {
+        client.close();
+      }
+      return null;
+    });
+  }
+
+  Future<String?> _recoverPassword(String name) {
+    return Future.delayed(loginTime).then((_) {
+      return null;
+    });
+  }
+
+  Future<String?> _signupConfirm(String error, LoginData data) {
+    return Future.delayed(loginTime).then((_) {
+      return null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      title: Constants.appName,
+      // logo: const AssetImage('assets/images/ecorp.png'),
+      // logoTag: Constants.logoTag,
+      titleTag: Constants.titleTag,
+      navigateBackAfterRecovery: true,
+      onConfirmRecover: _signupConfirm,
+      onConfirmSignup: _signupConfirm,
+      loginAfterSignUp: false,
+      loginProviders: [],
+      termsOfService: [],
+      additionalSignupFields: const [
+        UserFormField(
+            keyName: 'Username', icon: Icon(FontAwesomeIcons.userAlt)),
+        // const UserFormField(keyName: 'Name'),
+        // const UserFormField(keyName: 'Surname'),
+        // UserFormField(
+        //   keyName: 'phone_number',
+        //   displayName: 'Phone Number',
+        //   userType: LoginUserType.phone,
+        //   fieldValidator: (value) {
+        //     var phoneRegExp = RegExp(
+        //         '^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}\$');
+        //     if (value != null &&
+        //         value.length < 7 &&
+        //         !phoneRegExp.hasMatch(value)) {
+        //       return "This isn't a valid phone number";
+        //     }
+        //     return null;
+        //   },
+        // ),
+      ],
+      initialAuthMode: AuthMode.login,
+      // hideProvidersTitle: false,
+      // loginAfterSignUp: false,
+      // hideForgotPasswordButton: true,
+      // hideSignUpButton: true,
+      // disableCustomPageTransformer: true,
+      // messages: LoginMessages(
+      //   userHint: 'User',
+      //   passwordHint: 'Pass',
+      //   confirmPasswordHint: 'Confirm',
+      //   loginButton: 'LOG IN',
+      //   signupButton: 'REGISTER',
+      //   forgotPasswordButton: 'Forgot huh?',
+      //   recoverPasswordButton: 'HELP ME',
+      //   goBackButton: 'GO BACK',
+      //   confirmPasswordError: 'Not match!',
+      //   recoverPasswordIntro: 'Don\'t feel bad. Happens all the time.',
+      //   recoverPasswordDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+      //   recoverPasswordSuccess: 'Password rescued successfully',
+      //   flushbarTitleError: 'Oh no!',
+      //   flushbarTitleSuccess: 'Success!',
+      //   providersTitle: 'login with'
+      // ),
+      theme: LoginTheme(
+        primaryColor: const Color(0xffb01861),
+        accentColor: Colors.lightBlue[100],
+        errorColor: Colors.deepOrange,
+        pageColorLight: const Color(0xffECA8C2),
+        pageColorDark: const Color(0xff682045),
+        logoWidth: 0.80),
+      //   titleStyle: TextStyle(
+      //     color: Colors.greenAccent,
+      //     fontFamily: 'Quicksand',
+      //     letterSpacing: 4,
+      //   ),
+      //   // beforeHeroFontSize: 50,
+      //   // afterHeroFontSize: 20,
+      //   bodyStyle: TextStyle(
+      //     fontStyle: FontStyle.italic,
+      //     decoration: TextDecoration.underline,
+      //   ),
+      //   textFieldStyle: TextStyle(
+      //     color: Colors.orange,
+      //     shadows: [Shadow(color: Colors.yellow, blurRadius: 2)],
+      //   ),
+      //   buttonStyle: TextStyle(
+      //     fontWeight: FontWeight.w800,
+      //     color: Colors.yellow,
+      //   ),
+      //   cardTheme: CardTheme(
+      //     color: Colors.yellow.shade100,
+      //     elevation: 5,
+      //     margin: EdgeInsets.only(top: 15),
+      //     shape: ContinuousRectangleBorder(
+      //         borderRadius: BorderRadius.circular(100.0)),
+      //   ),
+      //   inputTheme: InputDecorationTheme(
+      //     filled: true,
+      //     fillColor: Colors.purple.withOpacity(.1),
+      //     contentPadding: EdgeInsets.zero,
+      //     errorStyle: TextStyle(
+      //       backgroundColor: Colors.orange,
+      //       color: Colors.white,
+      //     ),
+      //     labelStyle: TextStyle(fontSize: 12),
+      //     enabledBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.blue.shade700, width: 4),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     focusedBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.blue.shade400, width: 5),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     errorBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.red.shade700, width: 7),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     focusedErrorBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.red.shade400, width: 8),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     disabledBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.grey, width: 5),
+      //       borderRadius: inputBorder,
+      //     ),
+      //   ),
+      //   buttonTheme: LoginButtonTheme(
+      //     splashColor: Colors.purple,
+      //     backgroundColor: Colors.pinkAccent,
+      //     highlightColor: Colors.lightGreen,
+      //     elevation: 9.0,
+      //     highlightElevation: 6.0,
+      //     shape: BeveledRectangleBorder(
+      //       borderRadius: BorderRadius.circular(10),
+      //     ),
+      //     // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      //     // shape: CircleBorder(side: BorderSide(color: Colors.green)),
+      //     // shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(55.0)),
+      //   ),
+      // ),
+      userValidator: (value) {
+        if (!value!.contains('@')) {
+          return "Email must contain '@'";
+        }
+        return null;
+      },
+      passwordValidator: (value) {
+        if (value!.isEmpty) {
+          return 'Password is empty';
+        }
+        return null;
+      },
+      onLogin: (loginData) {
+        debugPrint('Login info');
+        debugPrint('Name: ${loginData.name}');
+        debugPrint('Password: ${loginData.password}');
+        return _loginUser(loginData);
+      },
+      onSignup: (signupData) {
+        debugPrint('Signup info');
+        debugPrint('Name: ${signupData.name}');
+        debugPrint('Password: ${signupData.password}');
+
+        signupData.additionalSignupData?.forEach((key, value) {
+          debugPrint('$key: $value');
+        });
+        if (signupData.termsOfService.isNotEmpty) {
+          debugPrint('Terms of service: ');
+          for (var element in signupData.termsOfService) {
+            debugPrint(
+                ' - ${element.term.id}: ${element.accepted == true ? 'accepted' : 'rejected'}');
+          }
+        }
+        return _signupUser(signupData);
+      },
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(FadePageRoute(
+          builder: (context) => const DashboardScreen(),
+        ));
+      },
+      onRecoverPassword: (name) {
+        debugPrint('Recover password info');
+        debugPrint('Name: $name');
+        return _recoverPassword(name);
+        // Show new password dialog
+      },
+      showDebugButtons: true,
+    );
+  }
+}
