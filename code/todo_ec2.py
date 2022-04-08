affabfcdacef9a4beafe977482a165df75154896
SoftFeta <mundanesurrealism@gmail.com>
2022-04-08 23:10:53 +0800

    Update to Boto3 code

--- code/todo_ec2.py
+++ code/todo_ec2.py
@@ -6,7 +6,6 @@ EC2 ImageIds:
 '''
 
 import boto3
-import botocore
 import paramiko
 from os.path import expanduser
 from time import time, sleep
@@ -26,7 +25,7 @@ client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 # Connect to EC2 Instance
-def connect(publicIp, instanceNumber, retries=5, master_ip=None):
+def connect(publicIp, instanceNumber, cctvUrl, retries=5, master_ip=None):
     global tm
     try:
         client.connect(hostname=publicIp, username="ec2-user", pkey=key, timeout=120)
@@ -49,7 +48,7 @@ def connect(publicIp, instanceNumber, retries=5, master_ip=None):
             print(f'Time needed to set up a GPU Instance: {time() - tm} s')
 
             # Start deep learning by issueing command to the container
-            stdin, stdout, stderr = client.exec_command('docker run --gpus=all --runtime=nvidia public.ecr.aws/o1i0p5x6/new bash -c "apt-get update;apt-get install -y libnvidia-ml-dev;/root/anaconda3/bin/conda run -n my-env python demo/demo_mot_vis.py configs/mot/tracktor/my2.py --input https://s81.ipcamlive.com/streams/514uqhm5v8lc0z9fj/stream.m3u8 --output "/tbd" --fps 5"')    #testsuite/unknown_02.mp4
+            stdin, stdout, stderr = client.exec_command(f'docker run --gpus=all --runtime=nvidia public.ecr.aws/o1i0p5x6/new bash -c "apt-get update;apt-get install -y libnvidia-ml-dev;/root/anaconda3/bin/conda run -n my-env python demo/demo_mot_vis.py configs/mot/tracktor/my2.py --input {cctvUrl} --output "/tbd" --fps 5"')    #testsuite/unknown_02.mp4
             print(stderr.read())
         else:
             stdin, stdout, stderr = client.exec_command(f'ping -c 4 {master_ip}')
@@ -72,52 +71,55 @@ def connect(publicIp, instanceNumber, retries=5, master_ip=None):
         print(f'{e}; Retrying...')
         if retries > 0:
             retries -= 1
-            connect(publicIp, instanceNumber, retries, master_ip)
+            connect(publicIp, instanceNumber, cctvUrl, retries, master_ip)
 
+def createG4Instance(cctvUrl='https://s81.ipcamlive.com/streams/514uqhm5v8lc0z9fj/stream.m3u8')
+    s = boto3.Session(
+        region_name=REGION_NAME)
+    ec2 = s.resource('ec2')
 
-s = boto3.Session(
-    region_name=REGION_NAME)
-ec2 = s.resource('ec2')
+    ##############################################################################
 
-##############################################################################
+    m = ec2.create_instances(ImageId=DEEP_LEARNING_AMAZON_LINUX,
+        InstanceType='g4dn.xlarge',
+        SecurityGroups=['launch-wizard-1'],
+        MaxCount=1,
+        KeyName=KEY_PAIR, 
+        MinCount=1,
+        BlockDeviceMappings=[{'DeviceName': '/dev/xvda', 'Ebs': { 'VolumeSize': 200 }}])
+    #print(dir(m[0]))
+    m[0].wait_until_running()
+    m[0].reload()
+    print(f'SSH into Master @{m[0].public_ip_address}...')
 
-m = ec2.create_instances(ImageId=DEEP_LEARNING_AMAZON_LINUX,
-    InstanceType='g4dn.xlarge',
-    SecurityGroups=['launch-wizard-1'],
-    MaxCount=1,
-    KeyName=KEY_PAIR, 
-    MinCount=1,
-    BlockDeviceMappings=[{"DeviceName": "/dev/xvda","Ebs" : { "VolumeSize" : 200 }}])
-#print(dir(m[0]))
-m[0].wait_until_running()
-m[0].reload()
-print(f'SSH into Master @{m[0].public_ip_address}...')
+    connect(m[0].public_ip_address, 0, cctvUrl)
 
-connect(m[0].public_ip_address, 0)
+    ##############################################################################
+    '''
+    w = ec2.create_instances(ImageId='ami-032ac9ac998363686',
+        InstanceType='t2.micro',
+        SecurityGroups=['launch-wizard-1'],
+        MaxCount=NUM_WORKERS,
+        KeyName=KEY_PAIR, 
+        MinCount=NUM_WORKERS)
+    for instance in range(len(w)):
+        w[instance].wait_until_running()
+        w[instance].reload()
 
-##############################################################################
-'''
-w = ec2.create_instances(ImageId='ami-032ac9ac998363686',
-    InstanceType='t2.micro',
-    SecurityGroups=['launch-wizard-1'],
-    MaxCount=NUM_WORKERS,
-    KeyName=KEY_PAIR, 
-    MinCount=NUM_WORKERS)
-for instance in range(len(w)):
-    w[instance].wait_until_running()
-    w[instance].reload()
+        print(f'SSH into Worker {instance} @{w[instance].public_ip_address}...')
 
-    print(f'SSH into Worker {instance} @{w[instance].public_ip_address}...')
+        connect(w[instance].public_ip_address, instance+1, master_ip=m[0].public_ip_address)
+    '''
+    ##############################################################################
 
-    connect(w[instance].public_ip_address, instance+1, master_ip=m[0].public_ip_address)
-'''
-##############################################################################
+    '''
+    #   Work is done
+    m[0].terminate()
+    # for i in w:
+    #     i.terminate()
+    m[0].wait_until_terminated()
+    # i.wait_until_terminated()
+    '''
 
-'''
-#   Work is done
-m[0].terminate()
-# for i in w:
-#     i.terminate()
-m[0].wait_until_terminated()
-# i.wait_until_terminated()
-'''
+if __name__ == '__main__':
+    createG4Instance()
